7d6f1e8ceedce4b2d675d793a9e5c92084dcd98d
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-05-26 17:22:14 -0400

    Formatted.

--- abjadext/microtones/__init__.py
+++ abjadext/microtones/__init__.py
@@ -1,11 +1,13 @@
 """
 Abjad's microtonal extension.
 """
-from ._version import __version__, __version_info__  # noqa
 from .EdoPitch import EdoPitch
-from .just_intonation import HEJIVector
-from .just_intonation import JIBundle
-from .just_intonation import is_prime
-from .just_intonation import prime_factors
-from .just_intonation import ratio_to_pc
-from .just_intonation import tune_to_ratio
+from ._version import __version__, __version_info__  # noqa
+from .just_intonation import (
+    HEJIVector,
+    JIBundle,
+    is_prime,
+    prime_factors,
+    ratio_to_pc,
+    tune_to_ratio,
+)
