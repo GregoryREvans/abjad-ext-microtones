fe2a811a19c4f859e3ffd44992a455a5cbce875a
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-05-26 16:42:58 -0400

    Decrufting.

--- abjadext/microtones/just_intonation.py
+++ abjadext/microtones/just_intonation.py
@@ -60,29 +60,7 @@ class HEJIVector:
         pass
 
     def calculate_heji_accidental(self):
-        vector = abjad.OrderedDict(
-            {
-                "syntonic comma(s) down": self.syntonic_commas_down,
-                "syntonic comma(s) up": self.syntonic_commas_up,
-                "septimal comma(s) down": self.septimal_commas_down,
-                "septimal comma(s) up": self.septimal_commas_up,
-                "undecimal quarter-tone(s) down": self.undecimal_quarter_tones_down,
-                "undecimal quarter-tone(s) up": self.undecimal_quarter_tones_up,
-                "tridecimal third-tone(s) down": self.tridecimal_third_tones_down,
-                "tridecimal third-tone(s) up": self.tridecimal_third_tones_up,
-                "seventeen-limit skhisma(s) down": self.seventeen_limit_skhismas_down,
-                "seventeen-limit skhisma(s) up": self.seventeen_limit_skhismas_up,
-                "nineteen-limit skhisma(s) down": self.nineteen_limit_skhismas_down,
-                "nineteen-limit skhisma(s) up": self.nineteen_limit_skhismas_up,
-                "twenty-three-limit comma(s) down": self.twenty_three_limit_commas_down,
-                "twenty-three-limit comma(s) up": self.twenty_three_limit_commas_up,
-            }
-        )
-        int_to_word = {
-            "1": "one",
-            "2": "two",
-            "3": "three",
-        }
+        int_to_word = {"1": "one", "2": "two", "3": "three"}
         accumulated_accidentals = []
         if self.diatonic_accidental == "double sharp":
             self.diatonic_accidental = "double-sharp"
@@ -97,13 +75,17 @@ class HEJIVector:
                 self.syntonic_commas_down - self.syntonic_commas_up
             )
             self.syntonic_commas_up = 0
-            accumulated_accidentals.append(fr"\forced-{self.diatonic_accidental}-{int_to_word[str(self.syntonic_commas_down)]}-syntonic-comma-down")
+            accumulated_accidentals.append(
+                fr"\forced-{self.diatonic_accidental}-{int_to_word[str(self.syntonic_commas_down)]}-syntonic-comma-down"
+            )
         else:
             self.syntonic_commas_up = (
                 self.syntonic_commas_up - self.syntonic_commas_down
             )
             self.syntonic_commas_down = 0
-            accumulated_accidentals.append(fr"\forced-{self.diatonic_accidental}-{int_to_word[str(self.syntonic_commas_up)]}-syntonic-comma-up")
+            accumulated_accidentals.append(
+                fr"\forced-{self.diatonic_accidental}-{int_to_word[str(self.syntonic_commas_up)]}-syntonic-comma-up"
+            )
         if self.septimal_commas_down == self.septimal_commas_up:
             self.septimal_commas_down = 0
             self.septimal_commas_up = 0
@@ -112,13 +94,17 @@ class HEJIVector:
                 self.septimal_commas_down - self.septimal_commas_up
             )
             self.septimal_commas_up = 0
-            accumulated_accidentals.append(fr"\{int_to_word[str(self.septimal_commas_down)]}-septimal-comma-down")
+            accumulated_accidentals.append(
+                fr"\{int_to_word[str(self.septimal_commas_down)]}-septimal-comma-down"
+            )
         else:
             self.septimal_commas_up = (
                 self.septimal_commas_up - self.septimal_commas_down
             )
             self.septimal_commas_down = 0
-            accumulated_accidentals.append(fr"\{int_to_word[str(self.septimal_commas_up)]}-septimal-comma-up")
+            accumulated_accidentals.append(
+                fr"\{int_to_word[str(self.septimal_commas_up)]}-septimal-comma-up"
+            )
         if self.undecimal_quarter_tones_down == self.undecimal_quarter_tones_up:
             self.undecimal_quarter_tones_down = 0
             self.undecimal_quarter_tones_up = 0
@@ -127,13 +113,17 @@ class HEJIVector:
                 self.undecimal_quarter_tones_down - self.undecimal_quarter_tones_up
             )
             self.undecimal_quarter_tones_up = 0
-            accumulated_accidentals.append(fr"\{int_to_word[str(self.undecimal_quarter_tones_down)]}-undecimal-quarter-tone-down")
+            accumulated_accidentals.append(
+                fr"\{int_to_word[str(self.undecimal_quarter_tones_down)]}-undecimal-quarter-tone-down"
+            )
         else:
             self.undecimal_quarter_tones_up = (
                 self.undecimal_quarter_tones_up - self.undecimal_quarter_tones_down
             )
             self.undecimal_quarter_tones_down = 0
-            accumulated_accidentals.append(fr"\{int_to_word[str(self.undecimal_quarter_tones_up)]}-undecimal-quarter-tone-up")
+            accumulated_accidentals.append(
+                fr"\{int_to_word[str(self.undecimal_quarter_tones_up)]}-undecimal-quarter-tone-up"
+            )
         if self.tridecimal_third_tones_down == self.tridecimal_third_tones_up:
             self.tridecimal_third_tones_down = 0
             self.tridecimal_third_tones_up = 0
@@ -142,13 +132,17 @@ class HEJIVector:
                 self.tridecimal_third_tones_down - self.tridecimal_third_tones_up
             )
             self.tridecimal_third_tones_up = 0
-            accumulated_accidentals.append(fr"\{int_to_word[str(self.tridecimal_third_tones_down)]}-tridecimal-third-tone-down")
+            accumulated_accidentals.append(
+                fr"\{int_to_word[str(self.tridecimal_third_tones_down)]}-tridecimal-third-tone-down"
+            )
         else:
             self.tridecimal_third_tones_up = (
                 self.tridecimal_third_tones_up - self.tridecimal_third_tones_down
             )
             self.tridecimal_third_tones_down = 0
-            accumulated_accidentals.append(fr"\{int_to_word[str(self.tridecimal_third_tones_up)]}-tridecimal-third-tone-up")
+            accumulated_accidentals.append(
+                fr"\{int_to_word[str(self.tridecimal_third_tones_up)]}-tridecimal-third-tone-up"
+            )
         if self.seventeen_limit_skhismas_down == self.seventeen_limit_skhismas_up:
             self.seventeen_limit_skhismas_down = 0
             self.seventeen_limit_skhismas_up = 0
@@ -157,13 +151,17 @@ class HEJIVector:
                 self.seventeen_limit_skhismas_down - self.seventeen_limit_skhismas_up
             )
             self.seventeen_limit_skhismas_up = 0
-            accumulated_accidentals.append(fr"\{int_to_word[str(self.seventeen_limit_skhismas_down)]}-seventeen-limit-skhisma-down")
+            accumulated_accidentals.append(
+                fr"\{int_to_word[str(self.seventeen_limit_skhismas_down)]}-seventeen-limit-skhisma-down"
+            )
         else:
             self.seventeen_limit_skhismas_up = (
                 self.seventeen_limit_skhismas_up - self.seventeen_limit_skhismas_down
             )
             self.seventeen_limit_skhismas_down = 0
-            accumulated_accidentals.append(fr"\{int_to_word[str(self.seventeen_limit_skhismas_up)]}-seventeen-limit-skhisma-up")
+            accumulated_accidentals.append(
+                fr"\{int_to_word[str(self.seventeen_limit_skhismas_up)]}-seventeen-limit-skhisma-up"
+            )
         if self.nineteen_limit_skhismas_down == self.nineteen_limit_skhismas_up:
             self.nineteen_limit_skhismas_down = 0
             self.nineteen_limit_skhismas_up = 0
@@ -172,13 +170,17 @@ class HEJIVector:
                 self.nineteen_limit_skhismas_down - self.nineteen_limit_skhismas_up
             )
             self.nineteen_limit_skhismas_up = 0
-            accumulated_accidentals.append(fr"\{int_to_word[str(self.nineteen_limit_skhismas_down)]}-nineteen-limit-skhisma-down")
+            accumulated_accidentals.append(
+                fr"\{int_to_word[str(self.nineteen_limit_skhismas_down)]}-nineteen-limit-skhisma-down"
+            )
         else:
             self.nineteen_limit_skhismas_up = (
                 self.nineteen_limit_skhismas_up - self.nineteen_limit_skhismas_down
             )
             self.nineteen_limit_skhismas_down = 0
-            accumulated_accidentals.append(fr"\{int_to_word[str(self.nineteen_limit_skhismas_up)]}-nineteen-limit-skhisma-up")
+            accumulated_accidentals.append(
+                fr"\{int_to_word[str(self.nineteen_limit_skhismas_up)]}-nineteen-limit-skhisma-up"
+            )
         if self.twenty_three_limit_commas_down == self.twenty_three_limit_commas_up:
             self.twenty_three_limit_commas_down = 0
             self.twenty_three_limit_commas_up = 0
@@ -187,26 +189,27 @@ class HEJIVector:
                 self.twenty_three_limit_commas_down - self.twenty_three_limit_commas_up
             )
             self.twenty_three_limit_commas_up = 0
-            accumulated_accidentals.append(fr"\{int_to_word[str(self.twenty_three_limit_commas_down)]}-twenty-three-limit-comma-down")
+            accumulated_accidentals.append(
+                fr"\{int_to_word[str(self.twenty_three_limit_commas_down)]}-twenty-three-limit-comma-down"
+            )
         else:
             self.twenty_three_limit_commas_up = (
                 self.twenty_three_limit_commas_up - self.twenty_three_limit_commas_down
             )
             self.twenty_three_limit_commas_down = 0
-            accumulated_accidentals.append(fr"\{int_to_word[str(self.twenty_three_limit_commas_up)]}-twenty-three-limit-comma-up")
+            accumulated_accidentals.append(
+                fr"\{int_to_word[str(self.twenty_three_limit_commas_up)]}-twenty-three-limit-comma-up"
+            )
         if len(accumulated_accidentals) > 0:
             accumulated_accidentals.reverse()
             literal_string_start = [
-                "\once \override Voice.Accidental.stencil =",
-                "       #ly:text-interface::print",
-                "       \once \override Voice.Accidental.text =",
-                "           \markup {",
-                "               \concat {",
+                r"\once \override Voice.Accidental.stencil =",
+                r"       #ly:text-interface::print",
+                r"       \once \override Voice.Accidental.text =",
+                r"           \markup {",
+                r"               \concat {",
             ]
-            literal_string_stop = [
-                "               }",
-                "           }",
-            ]
+            literal_string_stop = ["               }", "           }"]
             literal_components = []
             for literal_string in literal_string_start:
                 literal_components.append(literal_string)
@@ -217,16 +220,14 @@ class HEJIVector:
                 literal_components.append(literal_string)
             literal = abjad.LilyPondLiteral(literal_components, format_slot="before")
         else:
-            literal = abjad.LilyPondLiteral(fr"\forced-{self.diatonic_accidental}", format_slot="before")
+            literal = abjad.LilyPondLiteral(
+                fr"\forced-{self.diatonic_accidental}", format_slot="before"
+            )
         return literal
 
 
 class JIBundle:
-    def __init__(
-        self,
-        pitch="c'",
-        vector=HEJIVector(),
-    ):
+    def __init__(self, pitch="c'", vector=HEJIVector()):
         self.pitch = pitch
         self.vector = vector
 
@@ -332,7 +333,7 @@ def ratio_to_pc(pitch, ratio):
         else:
             print("REDUCE RATIO")
             print("cannot calculate beyond 23-limit JI")
-            assert(prime < 23)
+            assert prime < 23
     for prime in denominator_factors:
         if prime == 2:
             pitch = abjad.NamedInterval("-P8").transpose(pitch)
@@ -386,7 +387,7 @@ def ratio_to_pc(pitch, ratio):
         else:
             print("REDUCE RATIO")
             print("cannot calculate beyond 23-limit JI")
-            assert(prime < 23)
+            assert prime < 23
     accidental_vector.diatonic_accidental = pitch.accidental.name
     return JIBundle(pitch, accidental_vector)
 
@@ -409,13 +410,18 @@ def tune_to_ratio(note, ratio):
     selection = abjad.select(note).leaves()
     for note in selection:
         if abjad.inspect(note).annotation("ratio added") is not None:
-            ratio = Fraction(abjad.inspect(note).annotation("ratio added")) * Fraction(ratio)
+            ratio = Fraction(abjad.inspect(note).annotation("ratio added")) * Fraction(
+                ratio
+            )
             note.written_pitch = abjad.inspect(note).annotation("fundamental")
         abjad.annotate(note, "fundamental", f"{note.written_pitch}")
         abjad.annotate(note, "ratio added", f"{ratio}")
         note.written_pitch = ratio_to_pc(note.written_pitch, ratio).pitch
-    alteration_literal = ratio_to_pc(selection[0].written_pitch, ratio).vector.calculate_heji_accidental()
+    alteration_literal = ratio_to_pc(
+        selection[0].written_pitch, ratio
+    ).vector.calculate_heji_accidental()
     abjad.attach(alteration_literal, selection[0])
+
 
 ### TESTING ###
 
