fd1daa1b84b6b3e8223e4ea7b451ef6935976cc1
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-06-13 16:18:01 -0400

    Doctests.

--- abjadext/microtones/equal_temperament.py
+++ abjadext/microtones/equal_temperament.py
@@ -51,6 +51,7 @@ _value_to_accidental = {
     "5/3": r"\five-sixths-sharp",
     "8/5": r"\four-fifths-sharp",
     "3/2": r"\three-quarters-sharp",
+    "7/5": r"\seven-tenths-sharp",
     "4/3": r"\two-thirds-sharp",
     "5/4": r"\five-eighths-sharp",
     "6/5": r"\three-fifths-sharp",
@@ -60,17 +61,21 @@ _value_to_accidental = {
     "4/5": r"\two-fifths-sharp",
     "3/4": r"\three-eighths-sharp",
     "2/3": r"\one-third-sharp",
+    "3/5": r"\three-tenths-sharp",
     "1/2": r"\quarter-sharp",
     "2/5": r"\one-fifth-sharp",
     "1/3": r"\one-sixth-sharp",
     "1/4": r"\eighth-sharp",
+    "1/5": r"\one-tenth-sharp",
     "1/6": r"\one-twelfth-sharp",
     "0": r"\forced-natural",
     "-1/6": r"\one-twelfth-flat",
+    "-1/5": r"\one-tenth-flat",
     "-1/4": r"\eighth-flat",
     "-1/3": r"\one-sixth-flat",
     "-2/5": r"\one-fifth-flat",
     "-1/2": r"\quarter-flat",
+    "-3/5": r"\three-tenths-flat",
     "-2/3": r"\one-third-flat",
     "-3/4": r"\three-eighths-flat",
     "-4/5": r"\two-fifths-flat",
@@ -80,6 +85,7 @@ _value_to_accidental = {
     "-6/5": r"\three-fifths-flat",
     "-5/4": r"\five-eighths-flat",
     "-4/3": r"\two-thirds-flat",
+    "-7/5": r"\seven-tenths-flat",
     "-3/2": r"\three-quarters-flat",
     "-8/5": r"\four-fifths-flat",
     "-5/3": r"\five-sixths-flat",
@@ -203,6 +209,390 @@ def get_alteration(pitch, value):
 def apply_alteration(note_head, value):
     r"""
     Applies alteration.
+
+    ..  container:: example
+
+        >>> from fractions import Fraction
+        >>> steps = [0, "1/12", "1/8", "1/6", "1/5", "1/4", "1/3", "3/8", "2/5", "5/12", "1/2", "7/12", "3/5", "5/8", "2/3", "3/4", "4/5", "5/6", "7/8", "11/12", 1]
+        >>> steps = [Fraction(step) * 2 for step in steps]
+        >>> reverse_steps = [0 - step for step in steps]
+        >>> reverse_steps.reverse()
+        >>> total_steps = []
+        >>> total_steps.extend(reverse_steps)
+        >>> total_steps.extend(steps)
+        >>> final_steps = sorted(list(set(total_steps)))
+        >>> notes = [abjad.Note() for step in final_steps]
+        >>> for note, step in zip(notes, final_steps):
+        ...     microtones.apply_alteration(note.note_head, step)
+        >>> staff = abjad.Staff(notes)
+        >>> markup_fractions = [fraction / 2 for fraction in final_steps]
+        >>> pairs = [(abs(fraction.numerator), abs(fraction.denominator)) for fraction in markup_fractions]
+        >>> markups = [abjad.Markup(abjad.Markup.fraction(pair[0], pair[1]), direction=abjad.Up) for pair in pairs]
+        >>> for markup, note in zip(markups, abjad.select(staff).leaves()):
+        ...     abjad.attach(markup, note)
+        >>> abjad.attach(abjad.TimeSignature((5, 4)), abjad.select(staff).leaves()[0])
+        >>> abjad.attach(abjad.TimeSignature((1, 4)), abjad.select(staff).leaves()[20])
+        >>> abjad.attach(abjad.TimeSignature((5, 4)), abjad.select(staff).leaves()[21])
+        >>> break_points = [4, 9, 14, 19, 20, 25, 30, 35]
+        >>> for point in break_points:
+        ...     abjad.attach(abjad.LilyPondLiteral(r"\break", format_slot="after"), abjad.select(staff).leaves()[point])
+        >>> file = abjad.LilyPondFile.new(
+        ...     staff,
+        ...     includes=[
+        ...         "default.ily",
+        ...         "ekmelos-edo-accidental-markups.ily",
+        ...         "all-edo-markups-example.ily",
+        ...     ],
+        ... )
+        >>> style = '"dodecaphonic"'
+        >>> file.layout_block.items.append(fr"\accidentalStyle {style}")
+        >>> abjad.show(file) # doctest: +SKIP
+
+        ..  docs::
+
+            >>> abjad.f(staff)
+            \new Staff
+            {
+                \time 5/4
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \forced-flat-markup
+                bf4
+                ^ \markup {
+                    \fraction
+                        1
+                        1
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \five-twelfths-flat-markup
+                b4
+                ^ \markup {
+                    \fraction
+                        11
+                        12
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \three-eighths-flat-markup
+                b4
+                ^ \markup {
+                    \fraction
+                        7
+                        8
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-third-flat-markup
+                b4
+                ^ \markup {
+                    \fraction
+                        5
+                        6
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \three-tenths-flat-markup
+                b4
+                ^ \markup {
+                    \fraction
+                        4
+                        5
+                    }
+                \break
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \quarter-flat-markup
+                b4
+                ^ \markup {
+                    \fraction
+                        3
+                        4
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-sixth-flat-markup
+                b4
+                ^ \markup {
+                    \fraction
+                        2
+                        3
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \eighth-flat-markup
+                b4
+                ^ \markup {
+                    \fraction
+                        5
+                        8
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-tenth-flat-markup
+                b4
+                ^ \markup {
+                    \fraction
+                        3
+                        5
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-twelfth-flat-markup
+                b4
+                ^ \markup {
+                    \fraction
+                        7
+                        12
+                    }
+                \break
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \forced-natural-markup
+                b4
+                ^ \markup {
+                    \fraction
+                        1
+                        2
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \five-twelfths-flat-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        5
+                        12
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \two-fifths-flat-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        2
+                        5
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \three-eighths-flat-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        3
+                        8
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-third-flat-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        1
+                        3
+                    }
+                \break
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \quarter-flat-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        1
+                        4
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-fifth-flat-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        1
+                        5
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-sixth-flat-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        1
+                        6
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \eighth-flat-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        1
+                        8
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-twelfth-flat-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        1
+                        12
+                    }
+                \break
+                \time 1/4
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \forced-natural-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        0
+                        1
+                    }
+                \break
+                \time 5/4
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-twelfth-sharp-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        1
+                        12
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \eighth-sharp-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        1
+                        8
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-sixth-sharp-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        1
+                        6
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-fifth-sharp-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        1
+                        5
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \quarter-sharp-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        1
+                        4
+                    }
+                \break
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-third-sharp-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        1
+                        3
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \three-eighths-sharp-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        3
+                        8
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \two-fifths-sharp-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        2
+                        5
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \five-twelfths-sharp-markup
+                c'4
+                ^ \markup {
+                    \fraction
+                        5
+                        12
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \forced-flat-markup
+                df'4
+                ^ \markup {
+                    \fraction
+                        1
+                        2
+                    }
+                \break
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \five-twelfths-flat-markup
+                df'4
+                ^ \markup {
+                    \fraction
+                        7
+                        12
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \two-fifths-flat-markup
+                df'4
+                ^ \markup {
+                    \fraction
+                        3
+                        5
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \three-eighths-flat-markup
+                df'4
+                ^ \markup {
+                    \fraction
+                        5
+                        8
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-third-flat-markup
+                df'4
+                ^ \markup {
+                    \fraction
+                        2
+                        3
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \quarter-flat-markup
+                df'4
+                ^ \markup {
+                    \fraction
+                        3
+                        4
+                    }
+                \break
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-fifth-flat-markup
+                df'4
+                ^ \markup {
+                    \fraction
+                        4
+                        5
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-sixth-flat-markup
+                df'4
+                ^ \markup {
+                    \fraction
+                        5
+                        6
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \eighth-flat-markup
+                df'4
+                ^ \markup {
+                    \fraction
+                        7
+                        8
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \one-twelfth-flat-markup
+                df'4
+                ^ \markup {
+                    \fraction
+                        11
+                        12
+                    }
+                \tweak Accidental.stencil #ly:text-interface::print
+                \tweak Accidental.text \forced-natural-markup
+                d'4
+                ^ \markup {
+                    \fraction
+                        1
+                        1
+                    }
+            }
 
     ..  container:: example
 
--- abjadext/microtones/lilypond/edo-accidentals-test.ly
+++ abjadext/microtones/lilypond/edo-accidentals-test.ly
@@ -1,199 +1,246 @@
 \version "2.19.84"
 \language "english"
-#(set-default-paper-size "legallandscape")
+#(set-default-paper-size "letterportrait")
 #(set-global-staff-size 15)
 \include "ekmelos-edo-accidentals.ily"
+
+#(define (Text_align_engraver ctx)
+  (let ((scripts '())
+        (note-column #f))
+    (make-engraver
+     (acknowledgers
+      ((note-column-interface trans grob source)
+       ;; cache NoteColumn in this Voice context
+       (set! note-column grob))
+      ((text-script-interface trans grob source)
+       ;; whenever a TextScript is acknowledged,
+       ;; add it to `scripts' list
+       (set! scripts (cons grob scripts))))
+     ((stop-translation-timestep trans)
+      ;; if any TextScript grobs exist,
+      ;; set NoteColumn as X-parent
+      (for-each (lambda (script)
+		  (set! (ly:grob-parent script X) note-column))
+		scripts)
+      ;; clear scripts ready for next timestep
+      (set! scripts '())))))
+
+\header {tagline = ##f}
+
+\paper {
+  system-system-spacing = #'((basic-distance . 21) (minimum-distance . 21) (padding . 6))
+}
 
 \layout {
 	indent = #1
 	ragged-last = ##t
     ragged-right = ##t
 	\accidentalStyle "dodecaphonic"
-	\override Beam.transparent = ##t
-	\override Stem.transparent = ##t
+	\override Beam.stencil = ##f
+	\override Stem.stencil = ##f
 	\override Staff.BarLine.stencil = ##f
-	\override Flag.transparent = ##t
+	\override Flag.stencil = ##f
 	\override Staff.Clef.stencil = ##f
 	\override Staff.TimeSignature.stencil = ##f
-	\override Staff.StaffSymbol.transparent = ##t
+	\override Staff.StaffSymbol.stencil = ##f
 	\override SpacingSpanner.strict-grace-spacing = ##t
 	\override SpacingSpanner.uniform-stretching = ##t
 	\override SpacingSpanner.strict-note-spacing = ##t
     \override SpacingSpanner.uniform-stretching = ##t
 \context {
 	\Score
-	proportionalNotationDuration = #(ly:make-moment 1 8)
+	proportionalNotationDuration = #(ly:make-moment 1 10)
+	barNumberVisibility = ##f
 }
+  \context {
+    \Voice
+    \consists #Text_align_engraver
+    \override TextScript.X-offset =
+      #ly:self-alignment-interface::aligned-on-x-parent
+    \override TextScript.self-alignment-X = #CENTER
+  }
 }
 
 
 \new Score {
     <<
         \new Staff {
-		\time 3/4
+		\time 5/4
             \new Voice {
 				\double-flat
                 cff'4
-				^ \markup {\halign #0 - \fraction 1 1}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 1}
 
 				\eleven-twelfths-flat
                 ctqf'4
-				^ \markup {\halign #0 - \fraction 11 12}
+				^ \markup {\with-color #white .. \with-color #black \fraction 11 12}
 
 				\seven-eighths-flat
                 ctqf'4
-				^ \markup {\halign #0 - \fraction 7 8}
+				^ \markup {\with-color #white .. \with-color #black \fraction 7 8}
 
 				\five-sixths-flat
                 ctqf'4
-				^ \markup {\halign #0 - \fraction 5 6}
+				^ \markup {\with-color #white .. \with-color #black \fraction 5 6}
 
 				\four-fifths-flat
                 ctqf'4
-				^ \markup {\halign #0 - \fraction 4 5}
+				^ \markup {\with-color #white .. \with-color #black \fraction 4 5}
+				\break
 
 				\three-quarters-flat
                 ctqf'4
-				^ \markup {\halign #0 - \fraction 3 4}
+				^ \markup {\with-color #white .. \with-color #black \fraction 3 4}
 
 				\two-thirds-flat
                 cf'4
-				^ \markup {\halign #0 - \fraction 2 3}
+				^ \markup {\with-color #white .. \with-color #black \fraction 2 3}
 
 				\five-eighths-flat
                 cf'4
-				^ \markup {\halign #0 - \fraction 5 8}
+				^ \markup {\with-color #white .. \with-color #black \fraction 5 8}
 
 				\three-fifths-flat
                 cf'4
-				^ \markup {\halign #0 - \fraction 3 5}
+				^ \markup {\with-color #white .. \with-color #black \fraction 3 5}
 
 				\seven-twelfths-flat
                 cf'4
-				^ \markup {\halign #0 - \fraction 7 12}
+				^ \markup {\with-color #white .. \with-color #black \fraction 7 12}
+				\break
 
 				\forced-flat
                 cf'4
-				^ \markup {\halign #0 - \fraction 1 2}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 2}
 
 				\five-twelfths-flat
                 cqf'4
-				^ \markup {\halign #0 - \fraction 5 12}
+				^ \markup {\with-color #white .. \with-color #black \fraction 5 12}
 
 				\two-fifths-flat
                 cqf'4
-				^ \markup {\halign #0 - \fraction 2 5}
+				^ \markup {\with-color #white .. \with-color #black \fraction 2 5}
 
 				\three-eighths-flat
                 cqf'4
-				^ \markup {\halign #0 - \fraction 3 8}
+				^ \markup {\with-color #white .. \with-color #black \fraction 3 8}
 
 				\one-third-flat
                 cqf'4
-				^ \markup {\halign #0 - \fraction 1 3}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 3}
+				\break
 
 				\quarter-flat
                 cqf'4
-				^ \markup {\halign #0 - \fraction 1 4}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 4}
 
 				\one-fifth-flat
                 c'4
-				^ \markup {\halign #0 - \fraction 1 5}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 5}
 
 				\one-sixth-flat
                 c'4
-				^ \markup {\halign #0 - \fraction 1 6}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 6}
 
 				\one-eighth-flat
                 c'4
-				^ \markup {\halign #0 - \fraction 1 8}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 8}
 
 				\one-twelfth-flat
                 c'4
-				^ \markup {\halign #0 - \fraction 1 12}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 12}
+				\break
 
+				\time 1/4
 				\forced-natural
                 c'4
+				^ \markup {\with-color #white .. \with-color #black \fraction 0 1}
+				\break
 
+				\time 5/4
 				\one-twelfth-sharp
                 c'4
-				^ \markup {\halign #0 + \fraction 1 12}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 12}
 
 				\one-eighth-sharp
                 c'4
-				^ \markup {\halign #0 + \fraction 1 8}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 8}
 
 				\one-sixth-sharp
                 c'4
-				^ \markup {\halign #0 + \fraction 1 6}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 6}
 
 				\one-fifth-sharp
                 c'4
-				^ \markup {\halign #0 + \fraction 1 5}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 5}
 
 				\quarter-sharp
                 cqs'4
-				^ \markup {\halign #0 + \fraction 1 4}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 4}
+				\break
 
 				\one-third-sharp
                 cqs'4
-				^ \markup {\halign #0 + \fraction 1 3}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 3}
 
 				\three-eighths-sharp
                 cqs'4
-				^ \markup {\halign #0 + \fraction 3 8}
+				^ \markup {\with-color #white .. \with-color #black \fraction 3 8}
 
 				\two-fifths-sharp
                 cqs'4
-				^ \markup {\halign #0 + \fraction 2 5}
+				^ \markup {\with-color #white .. \with-color #black \fraction 2 5}
 
 				\five-twelfths-sharp
                 cqs'4
-				^ \markup {\halign #0 + \fraction 5 12}
+				^ \markup {\with-color #white .. \with-color #black \fraction 5 12}
 
 				\forced-sharp
                 cs'4
-				^ \markup {\halign #0 + \fraction 1 2}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 2}
+				\break
 
 				\seven-twelfths-sharp
                 cs'4
-				^ \markup {\halign #0 + \fraction 7 12}
+				^ \markup {\with-color #white .. \with-color #black \fraction 7 12}
 
 				\three-fifths-sharp
                 cs'4
-				^ \markup {\halign #0 + \fraction 3 5}
+				^ \markup {\with-color #white .. \with-color #black \fraction 3 5}
 
 				\five-eighths-sharp
                 cs'4
-				^ \markup {\halign #0 + \fraction 5 8}
+				^ \markup {\with-color #white .. \with-color #black \fraction 5 8}
 
 				\two-thirds-sharp
                 cs'4
-				^ \markup {\halign #0 + \fraction 2 3}
+				^ \markup {\with-color #white .. \with-color #black \fraction 2 3}
+				\break
 
 				\three-quarters-sharp
                 ctqs'4
-				^ \markup {\halign #0 + \fraction 3 4}
+				^ \markup {\with-color #white .. \with-color #black \fraction 3 4}
+				\break
 
 				\four-fifths-sharp
                 ctqs'4
-				^ \markup {\halign #0 + \fraction 4 5}
+				^ \markup {\with-color #white .. \with-color #black \fraction 4 5}
 
 				\five-sixths-sharp
                 ctqs'4
-				^ \markup {\halign #0 + \fraction 5 6}
+				^ \markup {\with-color #white .. \with-color #black \fraction 5 6}
 
 				\seven-eighths-sharp
                 ctqs'4
-				^ \markup {\halign #0 + \fraction 7 8}
+				^ \markup {\with-color #white .. \with-color #black \fraction 7 8}
 
 				\eleven-twelfths-sharp
                 ctqs'4
-				^ \markup {\halign #0 + \fraction 11 12}
+				^ \markup {\with-color #white .. \with-color #black \fraction 11 12}
 
 				\double-sharp
                 css'4
-				^ \markup {\halign #0 + \fraction 1 1}
+				^ \markup {\with-color #white .. \with-color #black \fraction 1 1}
             }
         }
     >>
